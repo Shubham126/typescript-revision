9e25aac5b38e28dba945da6c704cd80c1b37a105
Subham <chromaticshubham26@gmail.com>
2025-09-25 20:06:19 +0530

    function types

---
+++ 01intro/variable.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Shubham";
+greetings.toLocaleLowerCase();
+console.log(greetings);
+//number
+var userID = 334466;
+userID.toFixed();
+//boolean
+var isLoggedIn = false;
+var hero;
+function getHero() {
+    return "thor";
+}
+hero = getHero();
---
+++ 01intro/variable.ts
@@ -0,0 +1,24 @@
+let greetings: string = "Hello Shubham";
+greetings.toLocaleLowerCase()
+console.log(greetings);
+
+//number
+let userID: number = 334466
+userID.toFixed()
+
+//boolean
+
+let isLoggedIn: boolean = false
+
+let hero: string;
+
+function getHero(){
+    return "thor"
+}
+
+hero = getHero()
+
+
+
+
+export {}
